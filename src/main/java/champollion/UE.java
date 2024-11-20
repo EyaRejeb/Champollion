21c2dd19b17cc450aa383b1d068240c79de73818
EyaRejeb <93796426+EyaRejeb@users.noreply.github.com>
2024-11-20 23:56:55 +0100

    UE.java

--- src/main/java/champollion/UE.java
+++ src/main/java/champollion/UE.java
@@ -1,22 +1,15 @@
 package champollion;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class UE {
     private final String myIntitule;
 
-    private int heureCM;
-    private int heureTD;
-    private int heureTP;
-    private Set<Intervention> interventions = new HashSet<>();
-
-    public UE(String myIntitule) {
-        this.myIntitule = myIntitule;
+    public UE(String intitule) {
+        myIntitule = intitule;
     }
 
     public String getIntitule() {
         return myIntitule;
     }
 
+    
 }
